174eb7d616b38b390d25fc60d6d43f7b238cc807
Ashley Williams <ashley666ashley@gmail.com>
2016-08-26 17:03:31 -0400

    7.1.0

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const log = require('console-log-level')({ level: 'info' })
 const SassyMessage = require('./lib/SassyMessage');
 
 const msg = new SassyMessage("dependencies' prepublish scripts run on install");
-log.info(msg.print());
+log.warn(msg.print());
